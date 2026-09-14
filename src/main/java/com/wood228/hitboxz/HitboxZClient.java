18367665f010b75f878d650a61dd439d2d69e2d0
Wood228 <162610922+Wood228@users.noreply.github.com>
2026-09-14 16:53:43 +0500

    feat: add client entrypoint

---
+++ src/main/java/com/wood228/hitboxz/HitboxZClient.java
@@ -0,0 +1,12 @@
+package com.wood228.hitboxz;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public final class HitboxZClient implements ClientModInitializer {
+    public static final String MOD_ID = "hitboxz";
+
+    @Override
+    public void onInitializeClient() {
+        // Client initialization will be added as the hitbox system is implemented.
+    }
+}
