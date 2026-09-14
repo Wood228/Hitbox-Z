9941913ba34772e8bb3c485870728731fe1550d8
Wood228 <162610922+Wood228@users.noreply.github.com>
2026-09-14 18:03:32 +0500

    fix: clean HUD hotkey imports

--- src/main/java/com/wood228/hitboxz/HitboxZKeys.java
+++ src/main/java/com/wood228/hitboxz/HitboxZKeys.java
@@ -2,12 +2,9 @@ package com.wood228.hitboxz;
 
 import com.wood228.hitboxz.config.HitboxConfig;
 import com.wood228.hitboxz.gui.HitboxConfigScreen;
-import com.wood228.hitboxz.hud.HitboxHudConfig;
 import com.wood228.hitboxz.hud.HitboxHudEditorScreen;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.KeyMapping;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.text.Text;
